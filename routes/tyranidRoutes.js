a8e4be410e00e187828815b6d74cd87da9dd194f
Kieran Hart-Brooke <kieranhartbrooke@gmail.com>
2022-03-17 16:00:48 +0000

    finished crud

--- routes/tyranidRoutes.js
+++ routes/tyranidRoutes.js
@@ -11,31 +11,46 @@ router.get("/getAll", (req, res, next) => {
 
 });
 
-router.get("/get/:id", (req, res, next) => {
-    const id = Number.parseInt(req.params.id);
+router.get("/get/:id", ({params: {id}}, res, next) => {
+    Tyranid.findById(id, (err, found) => {
+        if (err)
+            return next({status: 400, message: err.message});
+        else if (!found)
+            return next({ status: 404, message: "No person found with id: " + id });
+        else
+            return res.send(found);
+    });
+})
 
-    Tyranid.find((err, tyranids) => {
+router.post("/create", ({body: tyranid}, res, next) => {
+    new Tyranid(tyranid).save()
+        .then(() => res.status(201).send("Successfully created"))
+        .catch(err => next({status: 400, message: err.message}));
+});
+
+router.put("/replace/:id", ({query: newTyranid, params: {id}}, res) => {
+    Tyranid.findByIdAndUpdate(id, newTyranid, (err, replaced) => {
         if (err)
             return next({status: 400, message: err.message});
         else 
-            return res.json(tyranids[id]);
+            Tyranid.findById(id, (err, updatedTyranid) => {
+                if (err)
+                    return next({status: 400, message: err.message});
+                else
+                    return res.status(202).send(updatedTyranid);
+            });
     })
-})
-
-router.post("/create", (req, res, next) => {
-    const tyranid = req.body;
-    
-    new Tyranid(tyranid).save().then(() => {
-        res.status(201).send("Successfully created");
-    }).catch(err => next({status: 400, message: err.message}));    
+   
 });
 
-router.put("/replace/:id", (req, res) => {
-    const newTyranid = req.query;
-    const id = Number.parseInt(req.params.id);   
-    
-    Tyranid.findOneAndReplace(id, newTyranid);
-    res.status(202).json(Tyranid[id]);
+router.delete("/remove/:id", ({params: {id}}, res) => {
+    Tyranid.findByIdAndDelete(id, (err) => {
+        if (err)
+            return next({status: 400, message: err.message});
+        else
+            return res.sendStatus(204);
+    })
 });
+
 
 module.exports = router;
